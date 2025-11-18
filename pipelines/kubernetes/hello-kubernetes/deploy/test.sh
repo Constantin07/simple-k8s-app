d66c71d36e6a79733d8a571af3a4b24d730b056e
Constantin Bugneac <constantin.bugneac@bjss.com>
2025-11-18 15:06:10 +0000

    Update pipelines and versions

--- pipelines/kubernetes/hello-kubernetes/deploy/test.sh
+++ pipelines/kubernetes/hello-kubernetes/deploy/test.sh
@@ -3,7 +3,7 @@
 SITE='https://hello-kubernetes.internal:30001'
 
 test_HelloKubernetesIsAccessible() {
-  result=`curl -sSfL --retry 5 -o /dev/null -w "%{http_code}" "$SITE"`
+  result="$(curl -sSfL --retry 5 -o /dev/null -w "%{http_code}" "$SITE")"
   assertEquals "Site is up" "${result}" '200'
 }
 
